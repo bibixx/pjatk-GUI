6a53c23201d907cc18dcf512dfa9d2916e4673f8
Bartosz Legięć <bartosz@legiec.eu>
2019-03-10 11:05:08 +0100

    [CW01.2] Remove arrayList from implementation

--- cw01/cw01.2/Main.java
+++ cw01/cw01.2/Main.java
@@ -1,21 +1,22 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 
 class Main {
   private static String[] filter(String[] arr, SFilter filtr) {
     int c = 0;
-    ArrayList<String> tmpArr = new ArrayList<String>();
+    String[] tmpArr = new String[arr.length];
 
     for (String arrElement : arr) {
       if (filtr.test(arrElement)) {
-        tmpArr.add(arrElement);
+        tmpArr[c] = arrElement;
         c++;
       }
     }
 
     String[] outArr = new String[c];
+    int len = c;
 
-    for (String arrElement : tmpArr) {
+    for (int i = 0; i < len; i++) {
+      String arrElement = tmpArr[i];
       outArr[outArr.length - c--] = arrElement;
     }
 
@@ -25,21 +26,31 @@ class Main {
   public static void main (String[] args) {
     String[] arr = {"Alice", "Sue", "Janet", "Bea"};
     System.out.println(Arrays.toString(arr));
+    System.out.println();
+
     LenFilter lenF = new LenFilter(3);
 
     String[] a1 = filter(arr, lenF);
-    System.out.println(Arrays.toString(a1));
-
+    System.out.println("#1");
+    System.out.println("Got:      " + Arrays.toString(a1));
+    System.out.println("Expected: [Alice, Janet]");
+    System.out.println();
+    
     String[] a2 = filter(arr, new SFilter(){
       @Override
       public boolean test(String s) {
         return s.charAt(0) < 'D';
       }
     });
-
-    System.out.println(Arrays.toString(a2));
-
+    
+    System.out.println("#2");
+    System.out.println("Got:      " + Arrays.toString(a2));
+    System.out.println("Expected: [Alice, Bea]");
+    System.out.println();
+    
     String[] a3 = filter(arr, (String s) -> s.charAt(0) > 'H');
-    System.out.println(Arrays.toString(a3));
+    System.out.println("#3");
+    System.out.println("Got:      " + Arrays.toString(a3));
+    System.out.println("Expected: [Sue, Janet]");
   }
 }
