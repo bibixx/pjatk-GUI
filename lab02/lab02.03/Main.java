a6a12efba1e9b879ff02379ed8809e43627d14d1
Bartosz Legięć <bartosz@legiec.eu>
2019-03-26 19:15:12 +0100

    [LAB02.03] Add prompt

--- lab02/lab02.03/Main.java
+++ lab02/lab02.03/Main.java
@@ -1,12 +1,57 @@
 import java.util.*;
+import javax.swing.*;
 
 class Main {
+  private static Map<String, String> showDialog() {
+    JTextField nameField = new JTextField(5);
+    JTextField birthyearField = new JTextField(5);
+    JPanel myPanel = new JPanel();
+
+    BoxLayout layout = new BoxLayout(myPanel, BoxLayout.PAGE_AXIS);
+    myPanel.setLayout(layout);
+
+    myPanel.add(new JLabel("Imię"));
+    myPanel.add(nameField);
+    myPanel.add(Box.createHorizontalStrut(15));
+    myPanel.add(new JLabel("Data urodzenia"));
+    myPanel.add(birthyearField);
+
+    int result = JOptionPane.showConfirmDialog(
+      null,
+      myPanel,
+      "Podaj dane",
+      JOptionPane.OK_CANCEL_OPTION
+    );
+
+    if (result == JOptionPane.OK_OPTION) {
+      Map<String, String> map = new HashMap<String, String>();
+      map.put("name", nameField.getText());
+      map.put("birthYear", birthyearField.getText());
+
+      return map;
+    }
+
+    return null;
+  }
+
   public static void main(String[] argv) {
     ArrayList<Person> list = new ArrayList<Person>();
+    Map<String, String> newPerson;
+    
+    while ((newPerson = showDialog()) != null) {
+      int year;
 
-    list.add(new Person("Adam", 2009));
-    list.add(new Person("Janek", 2008));
-    list.add(new Person("Daniel", 1989));
+      try {
+        year = Integer.parseInt(newPerson.get("birthYear"));
+      } catch (NumberFormatException e) {
+        continue;
+      }
+
+      list.add(new Person(
+        newPerson.get("name"),
+        year
+      ));
+    };
 
     HashSet<Person> set = new HashSet<Person>(list);
 
